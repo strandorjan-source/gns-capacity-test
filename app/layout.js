1bb9e51417c14a7aa133e01e0f22c6a1fda2e56f
strandorjan-source <strand.orjan@gmail.com>
2026-09-15 12:33:26 +0200

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'GNS Capacity',description:'Ledige biler – GNS Cargo AS'};
+export default function RootLayout({children}){return <html lang="no"><body>{children}</body></html>}
